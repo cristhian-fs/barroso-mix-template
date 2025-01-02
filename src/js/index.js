d9cbcd7ca21a18ef6889722cf5e8949d4a97f269
Cristhian <cristhianfernandolp@gmail.com>
2025-01-02 11:31:37 -0300

    feat: add function to handle menu states

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,41 @@
+function initNavbar() {
+  const sidebarNavButtons = document.querySelectorAll('.sidebar_navigation_item, .sidebar_social_link');
+  const sidebarWrapper = document.querySelector('.sidebar_navigation_wrapper');
+  const burgerMenuBtn = document.querySelector('.burger input');
+  const sidebarClose = document.querySelector('.sidebar_close');
+  let isOpen = false;
+
+  function openSidebar() {
+    sidebarWrapper.setAttribute('data-state', 'open');
+    isOpen = true;
+  }
+
+  function closeSidebar() {
+    sidebarWrapper.setAttribute('data-state', 'closed');
+    isOpen = false;
+    burgerMenuBtn.checked = false;
+  }
+
+  burgerMenuBtn.addEventListener('click', openSidebar);
+
+  sidebarNavButtons.forEach(btn => {
+    btn.addEventListener('click', closeSidebar);
+  });
+
+  document.addEventListener('click', e => {
+    if (e.target.closest('.sidebar_overlay') && isOpen) {
+      closeSidebar();
+    }
+  });
+}
+
+function setNavbarHeightStyle(){
+  const navbarHeight = document.querySelector('.sidebar').getBoundingClientRect();
+  document.documentElement.style.setProperty('--navbar-h', `${navbarHeight.height + navbarHeight.top}px`);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  const navbarHeight = document.querySelector('.sidebar').getBoundingClientRect();
-
-  document.documentElement.style.setProperty('--navbar-h', `${navbarHeight.height + navbarHeight.top}px`);
-})
+  setNavbarHeightStyle();
+  initNavbar()
+});
